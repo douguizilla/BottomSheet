6fa68804deafb96cf96d5e538346c25cb37225ac
douguizilla <douglasgommez@gmail.com>
2021-08-23 19:01:48 -0300

    set btnBottomShet to set Bottom sheet state

--- app/src/main/java/com/odougle/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/odougle/bottomsheet/MainActivity.kt
@@ -2,7 +2,9 @@ package com.odougle.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.odougle.bottomsheet.databinding.ActivityMainBinding
+import com.odougle.bottomsheet.databinding.LayoutBottomSheetBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +12,22 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val layoutBottomSheetBinding: LayoutBottomSheetBinding by lazy {
+        LayoutBottomSheetBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btnBottomSheet.setOnClickListener {
+            val behavior = BottomSheetBehavior.from(layoutBottomSheetBinding.layoutBottomSheet)
+            if(behavior.state == BottomSheetBehavior.STATE_HIDDEN ||
+                behavior.state == BottomSheetBehavior.STATE_EXPANDED){
+                behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }else if(behavior.state == BottomSheetBehavior.STATE_COLLAPSED){
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
     }
 }
